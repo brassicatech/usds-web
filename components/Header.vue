c1ff1df951c8b9eb8e810ed82e8e78d49c118696
emodot <emayeodavid@gmail.com>
2024-09-15 11:04:36 +0100

    code setup

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex">
+    <div><img src="@/assets/images/logo.png" alt=""></div>
+    <div><p>SETTING THE NEW STANDARD</p></div>
+    <div><p>Q1 2025 LAUNCH</p></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
